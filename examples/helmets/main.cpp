65a7d6bf2ab9ea97419b6e7235b7b066f3c6b1ec
Christoph Fleschutz <ch.fleschutz@gmail.com>
2026-02-11 22:04:57 +0100

    Remove helmet instances

--- examples/helmets/main.cpp
+++ examples/helmets/main.cpp
@@ -60,38 +60,6 @@ public:
 		auto scifiHelmet = scene.load(ASSETS_DIR "SciFiHelmet/ScifiHelmet.gltf");
 		scifiHelmet.get<Transform>().location = { 2.0f, 0.0f, 2.0f };
 
-		Scene::Entity meshEntity = scifiHelmet;
-		std::shared_ptr<Graphics::StaticMesh> mesh;
-		std::shared_ptr<Graphics::MaterialInstance> materialInstance;
-		while (!meshEntity.has<Mesh, Material>())
-		{
-			meshEntity = meshEntity.get<Children>().last;
-			AGX_ASSERT_X(meshEntity, "Failed to find mesh and material in SciFiHelmet scene");
-		}
-		mesh = meshEntity.get<Mesh>().staticMesh;
-		materialInstance = meshEntity.get<Material>().instance;
-		AGX_ASSERT(mesh && materialInstance);
-
-		constexpr int instanceCount = 9'000;
-		constexpr float boxSize = 100;
-
-		auto dis = std::uniform_real_distribution<float>(-boxSize, boxSize);
-		for (int i = 0; i < instanceCount; i++)
-		{
-			auto instance = scene.createEntity("SciFiHelmetInstance");
-			instance.add<Mesh>(mesh);
-			instance.add<Material>(materialInstance);
-			auto& transform = instance.get<Transform>();
-			transform.location = {
-				dis(Random::generator()),
-				dis(Random::generator()),
-				dis(Random::generator()) / 2.0f
-			};
-			transform.rotation = glm::radians(glm::vec3{ 90.0f, 0.0f, 0.0f });
-			float scale = static_cast<float>(rand() % 50 + 50) / 100.0f;
-			transform.scale = { scale, scale, scale };
-		}
-
 		auto plane = scene.load(ASSETS_DIR "Misc/plane.obj");
 		plane.get<Transform>().scale = { 2.0f, 2.0f, 2.0f };
 		plane.add<ColorChanger>();
