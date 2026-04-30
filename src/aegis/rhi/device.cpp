5639f6165f6f9219290192bff429d56dbc6b8260
Christoph Fleschutz <ch.fleschutz@gmail.com>
2026-04-30 18:17:20 +0200

    Rework physical device selection

--- src/aegis/rhi/device.cpp
+++ src/aegis/rhi/device.cpp
@@ -4,6 +4,7 @@ module;
 
 #include <set>
 #include <vector>
+#include <algorithm>
 
 module aegis.rhi;
 import :device;
@@ -15,7 +16,7 @@ Device::Device(const Desc& desc)
     createInstance(desc);
     m_surface = vk::raii::SurfaceKHR{ m_instance, desc.createSurface(*m_instance) };
     createPhysicalDevice(desc);
-    createDevice(desc);
+    // createDevice(desc);
 }
 
 void Device::createInstance(const Desc& desc)
@@ -38,13 +39,13 @@ void Device::createInstance(const Desc& desc)
         .ppEnabledExtensionNames = extensions.data(),
     };
 
-    auto instanceRes = m_context.createInstance(instanceInfo);
-    if (instanceRes.result != vk::Result::eSuccess)
+    auto [result, instance] = m_context.createInstance(instanceInfo);
+    if (result != vk::Result::eSuccess)
     {
         // TODO: log error
         return;
     }
-    m_instance = std::move(instanceRes.value);
+    m_instance = std::move(instance);
 }
 
 void Device::createPhysicalDevice(const Desc& desc)
@@ -56,90 +57,68 @@ void Device::createPhysicalDevice(const Desc& desc)
         return;
     }
 
-    for (auto& physicalDevice : physicalDevices)
+    std::vector<std::pair<uint32_t, uint32_t>> candidates;
+    for (size_t i = 0; i < physicalDevices.size(); ++i)
     {
+        const auto& physicalDevice = physicalDevices[i];
+        uint32_t score{ 0 };
+
         if (!findQueueFamilies(physicalDevice).isComplete())
             continue;
 
-        auto featureChain = physicalDevice.getFeatures2<vk::PhysicalDeviceFeatures2,
+        auto features = physicalDevice.getFeatures2< //
+            vk::PhysicalDeviceFeatures2,
             vk::PhysicalDeviceVulkan11Features,
             vk::PhysicalDeviceVulkan12Features,
             vk::PhysicalDeviceVulkan13Features,
             vk::PhysicalDeviceMeshShaderFeaturesEXT>();
+        const auto& vk10features = features.get<vk::PhysicalDeviceFeatures2>();
+        const auto& vk11features = features.get<vk::PhysicalDeviceVulkan11Features>();
+        const auto& vk12features = features.get<vk::PhysicalDeviceVulkan12Features>();
+        const auto& vk13features = features.get<vk::PhysicalDeviceVulkan13Features>();
+        const auto& meshShaderFeatures = features.get<vk::PhysicalDeviceMeshShaderFeaturesEXT>();
+        // TODO: Check features
 
-        const auto& vk10features = featureChain.get<vk::PhysicalDeviceFeatures2>();
-        const auto& vk11features = featureChain.get<vk::PhysicalDeviceVulkan11Features>();
-        const auto& vk12features = featureChain.get<vk::PhysicalDeviceVulkan12Features>();
-        const auto& vk13features = featureChain.get<vk::PhysicalDeviceVulkan13Features>();
-        const auto& meshShaderFeatures = featureChain.get<vk::PhysicalDeviceMeshShaderFeaturesEXT>();
+        auto availableExtensions = physicalDevice.enumerateDeviceExtensionProperties();
+        // TODO: Check extensions
 
-        if (!vk10features.features.samplerAnisotropy)
+        const auto properties = physicalDevice.getProperties();
+        if (properties.apiVersion < vk::ApiVersion13)
             continue;
 
-        if (!vk11features.shaderDrawParameters)
-            continue;
+        if (properties.deviceType == vk::PhysicalDeviceType::eDiscreteGpu)
+            score += 1000;
 
-        // Bindless
-        if (!vk12features.descriptorIndexing || !vk12features.shaderUniformBufferArrayNonUniformIndexing ||
-            !vk12features.shaderSampledImageArrayNonUniformIndexing ||
-            !vk12features.shaderStorageBufferArrayNonUniformIndexing ||
-            !vk12features.shaderStorageImageArrayNonUniformIndexing ||
-            !vk12features.descriptorBindingUniformBufferUpdateAfterBind ||
-            !vk12features.descriptorBindingSampledImageUpdateAfterBind ||
-            !vk12features.descriptorBindingStorageImageUpdateAfterBind ||
-            !vk12features.descriptorBindingStorageBufferUpdateAfterBind || !vk12features.descriptorBindingPartiallyBound ||
-            !vk12features.descriptorBindingVariableDescriptorCount || !vk12features.runtimeDescriptorArray)
-            continue;
-
-        // Buffer layouts
-        if (!vk12features.uniformBufferStandardLayout || !vk12features.scalarBlockLayout)
-            continue;
-
-        // 8-bit storage
-        if (!vk12features.shaderInt8 || !vk12features.storageBuffer8BitAccess ||
-            !vk12features.uniformAndStorageBuffer8BitAccess || !vk12features.storagePushConstant8)
-            continue;
-
-        if (!vk13features.dynamicRendering || !vk13features.maintenance4 || !vk13features.shaderDemoteToHelperInvocation)
-            continue;
-
-        if (!meshShaderFeatures.meshShader || !meshShaderFeatures.taskShader)
-            continue;
-
-        auto propsChain =
-            physicalDevice.getProperties2<vk::PhysicalDeviceProperties2, vk::PhysicalDeviceVulkan11Properties, vk::PhysicalDeviceVulkan12Properties, vk::PhysicalDeviceVulkan13Properties>();
-
-        const auto& coreProps = propsChain.get<vk::PhysicalDeviceProperties2>();
-        const auto& vk11Props = propsChain.get<vk::PhysicalDeviceVulkan11Properties>();
-        const auto& vk12Props = propsChain.get<vk::PhysicalDeviceVulkan12Properties>();
-        const auto& vk13Props = propsChain.get<vk::PhysicalDeviceVulkan13Properties>();
-
-        m_properties = Properties{
-            .core = coreProps,
-            .vk11 = vk11Props,
-            .vk12 = vk12Props,
-            .vk13 = vk13Props,
-        };
-
-        m_capabilities =
-            Capabilities{ .meshShaders = meshShaderFeatures.meshShader && meshShaderFeatures.taskShader };
-
-        // TODO: Maybe select based on some score instead of just using the first
-        // Also prefer discrete gpu (Here its possible to end up with integrated even if discrete is present)
-        m_physicalDevice = physicalDevice;
-        break;
+        score += properties.limits.maxImageDimension2D;
+        candidates.emplace_back(score, i);
     }
+
+    std::ranges::sort(candidates);
+    if (candidates.empty() || candidates.front().first == 0)
+    {
+        // TODO: Error
+        return;
+    }
+
+    auto [_, index] = candidates.front();
+    m_physicalDevice = physicalDevices[index];
 }
 
 void Device::createDevice(const Desc& desc)
 {
-    vk::StructureChain<vk::DeviceCreateInfo, vk::PhysicalDeviceFeatures2, vk::PhysicalDeviceVulkan11Features,
-    vk::PhysicalDeviceVulkan12Features, vk::PhysicalDeviceVulkan13Features, vk::PhysicalDeviceMeshShaderFeaturesEXT>
+    vk::StructureChain<vk::DeviceCreateInfo,
+        vk::PhysicalDeviceFeatures2,
+        vk::PhysicalDeviceVulkan11Features,
+        vk::PhysicalDeviceVulkan12Features,
+        vk::PhysicalDeviceVulkan13Features,
+        vk::PhysicalDeviceMeshShaderFeaturesEXT>
         deviceChain;
 
     deviceChain.get<vk::PhysicalDeviceFeatures2>().features.setSamplerAnisotropy(true);
 
-    deviceChain.get<vk::PhysicalDeviceVulkan11Features>().setShaderDrawParameters(true);
+    deviceChain
+        .get<vk::PhysicalDeviceVulkan11Features>() //
+        .setShaderDrawParameters(true);
 
     deviceChain.get<vk::PhysicalDeviceVulkan12Features>()
         .setStorageBuffer8BitAccess(true)
@@ -167,7 +146,10 @@ void Device::createDevice(const Desc& desc)
         .setDynamicRendering(true)
         .setMaintenance4(true);
 
-    deviceChain.get<vk::PhysicalDeviceMeshShaderFeaturesEXT>().setMeshShader(true).setTaskShader(true);
+    deviceChain
+        .get<vk::PhysicalDeviceMeshShaderFeaturesEXT>() //
+        .setMeshShader(true)
+        .setTaskShader(true);
 
     auto extensions = std::array{
         vk::KHRSwapchainExtensionName,
@@ -188,9 +170,11 @@ void Device::createDevice(const Desc& desc)
         });
     }
 
-    deviceChain.get<vk::DeviceCreateInfo>().setQueueCreateInfos(queueCreateInfos).setPEnabledExtensionNames(extensions);
+    deviceChain.get<vk::DeviceCreateInfo>()
+        .setQueueCreateInfos(queueCreateInfos)
+        .setPEnabledExtensionNames(extensions);
 
-    auto [result, device] = m_physicalDevice.createDevice(deviceChain.get<vk::DeviceCreateInfo>());
+    m_device = vk::raii::Device(m_physicalDevice, deviceChain.get<vk::PhysicalDevice>());
     if (result != vk::Result::eSuccess)
     {
         // TODO: error
@@ -209,15 +193,17 @@ auto Device::findQueueFamilies(const vk::PhysicalDevice& physicalDevice) const -
     // for (uint32_t i = 0; i < queueFamilies.size(); ++i)
     // {
     //     const auto& props = queueFamilies[i];
-    //     const bool hasGraphics = static_cast<bool>(props.queueFlags & vk::QueueFlagBits::eGraphics);
-    //     const bool hasCompute = static_cast<bool>(props.queueFlags & vk::QueueFlagBits::eCompute);
-    //     const bool hasTransfer = static_cast<bool>(props.queueFlags & vk::QueueFlagBits::eTransfer);
+    //     const bool hasGraphics = static_cast<bool>(props.queueFlags &
+    //     vk::QueueFlagBits::eGraphics); const bool hasCompute = static_cast<bool>(props.queueFlags
+    //     & vk::QueueFlagBits::eCompute); const bool hasTransfer =
+    //     static_cast<bool>(props.queueFlags & vk::QueueFlagBits::eTransfer);
     //
     //     auto [result, hasPresent] = physicalDevice.getSurfaceSupportKHR(i, *m_surface);
     //     if (result != vk::Result::eSuccess)
     //         continue;
     //
-    //     if (hasGraphics && hasCompute && hasTransfer && indices.graphics == vk::QueueFamilyIgnored)
+    //     if (hasGraphics && hasCompute && hasTransfer && indices.graphics ==
+    //     vk::QueueFamilyIgnored)
     //         indices.graphics = i;
     //
     //     if (hasPresent && indices.present == vk::QueueFamilyIgnored)
