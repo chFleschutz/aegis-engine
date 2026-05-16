667a4d36edceb1f18f0a5f7a25cd9d17e8e6cbf7
Christoph Fleschutz <ch.fleschutz@gmail.com>
2026-05-16 19:44:39 +0200

    Add move semantics to window

--- src/aegis/platform/window.cpp
+++ src/aegis/platform/window.cpp
@@ -8,46 +8,64 @@ module aegis.platform.window;
 
 namespace aegis::platform
 {
-	Window::Window(const Desc& desc) :
-		m_width{ desc.width }, m_height{ desc.height }
-	{
-		glfwInit();
-		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+Window::Window(const Desc& desc) :
+    m_width{ desc.width },
+    m_height{ desc.height }
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-		m_window = glfwCreateWindow(static_cast<int>(desc.width),
-		                            static_cast<int>(desc.height),
-		                            desc.title.data(),
-		                            nullptr,
-		                            nullptr);
-		assert(m_window && "Platform Error: Failed to create window");
+    m_window = glfwCreateWindow(static_cast<int>(desc.width),
+        static_cast<int>(desc.height),
+        desc.title.data(),
+        nullptr,
+        nullptr);
+    assert(m_window && "Platform Error: Failed to create window");
 
-		glfwSetWindowUserPointer(m_window, this);
-		glfwSetWindowSizeCallback(m_window, onWindowResize);
-	}
+    glfwSetWindowUserPointer(m_window, this);
+    glfwSetWindowSizeCallback(m_window, onWindowResize);
+}
 
-	Window::~Window()
-	{
-		glfwDestroyWindow(m_window);
-		glfwTerminate();
-	}
+Window::Window(Window&& other) noexcept
+{
+    std::swap(m_window, other.m_window);
+    if (m_window)
+        glfwSetWindowUserPointer(m_window, this);
+}
 
-	auto Window::shouldClose() const -> bool
-	{
-		return glfwWindowShouldClose(m_window);
-	}
-
-    auto Window::pollEvents() const
-        -> void
+Window::~Window()
+{
+    if (m_window)
     {
-	    glfwPollEvents();
+        glfwDestroyWindow(m_window);
+        glfwTerminate();
     }
+}
 
-    void Window::onWindowResize(GLFWwindow* glfwWindow, int newWidth, int newHeight)
-	{
-		const auto window = static_cast<Window*>(glfwGetWindowUserPointer(glfwWindow));
-		window->m_width = static_cast<uint32_t>(newWidth);
-		window->m_height = static_cast<uint32_t>(newHeight);
-		window->m_wasResized = true;
-	}
+auto Window::operator=(Window&& other) noexcept -> Window&
+{
+    std::swap(m_window, other.m_window);
+    if (m_window)
+        glfwSetWindowUserPointer(m_window, this);
+    return *this;
+}
+
+auto Window::shouldClose() const -> bool
+{
+    return glfwWindowShouldClose(m_window);
+}
+
+auto Window::pollEvents() const -> void
+{
+    glfwPollEvents();
+}
+
+auto Window::onWindowResize(GLFWwindow* glfwWindow, int newWidth, int newHeight) -> void
+{
+    const auto window = static_cast<Window*>(glfwGetWindowUserPointer(glfwWindow));
+    window->m_width = static_cast<uint32_t>(newWidth);
+    window->m_height = static_cast<uint32_t>(newHeight);
+    window->m_wasResized = true;
+}
 }
--- src/aegis/platform/window.cppm
+++ src/aegis/platform/window.cppm
@@ -18,7 +18,12 @@ public:
     };
 
     explicit Window(const Desc& desc);
+    Window(const Window&) = delete;
+    Window(Window&& other) noexcept;
     ~Window();
+
+    auto operator=(const Window&) -> Window& = delete;
+    auto operator=(Window&& other) noexcept -> Window&;
 
     [[nodiscard]] auto glfwWindow() const -> GLFWwindow* { return m_window; }
     [[nodiscard]] auto width() const -> uint32_t { return m_width; }
@@ -29,7 +34,7 @@ public:
     auto pollEvents() const -> void;
 
 private:
-    static void onWindowResize(GLFWwindow* glfwWindow, int newWidth, int newHeight);
+    static auto onWindowResize(GLFWwindow* glfwWindow, int newWidth, int newHeight) -> void;
 
     GLFWwindow* m_window{ nullptr };
     uint32_t m_width{ 0 };
